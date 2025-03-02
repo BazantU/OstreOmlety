531981e6bbbdf4fcd80c26e31ddc9b44b7fb2512
Wfel-0 <125511279+Wfel-0@users.noreply.github.com>
2025-03-02 16:51:42 +0100

    nie działa

--- Assets/Scripts/LaptopikWos.cs
+++ Assets/Scripts/LaptopikWos.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class LaptopikWos : MonoBehaviour
 {
@@ -10,6 +12,11 @@ public class LaptopikWos : MonoBehaviour
     public GameObject salceson;
     public GameObject komunikat1;
     public GameObject komunikat2;
+    public GameObject cam;
+    public bool pokazLaptoka;
+    public GameObject text;
+    public TMP_InputField text1;
+    public bool pokazLaptoka2;
 
     Vector3 odleglosc;
     bool rozwiazano;
@@ -23,39 +30,130 @@ public class LaptopikWos : MonoBehaviour
         rozwiazano = false;
     }
 
+
+    private void OnMouseExit(){
+        mouseOver = false;
+    }
+
+    private void OnMouseOver()
+    {
+        if(odleglosc.magnitude < odlegloscPodnoszenia){
+            transform.gameObject.layer = LayerMask.NameToLayer("Outline");
+        }
+        mouseOver = true;
+
+        if (Input.GetKeyDown(KeyCode.E) && odleglosc.magnitude <= odlegloscPodnoszenia && !rozwiazano && PendriveDoLaptopikaWos.trzymany)
+        {
+            PendriveDoLaptopikaWos.trzymany2 = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.E) && odleglosc.magnitude <= odlegloscPodnoszenia && !PendriveDoLaptopikaWos.trzymany && PendriveDoLaptopikaWos.udaloSie && !pokazLaptoka)
+        {
+            pokazLaptoka = true;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.Confined;
+            player.GetComponent<Player>().enabled = false;
+            cam.GetComponent<SC_HeadBobber>().enabled = false;
+            
+        }
+    }
+
+    // Update is called once per frame
     void Update()
     {
-        if(odleglosc.magnitude > odlegloscPodnoszenia || !mouseOver)
-        {
+        if(odleglosc.magnitude > odlegloscPodnoszenia || !mouseOver){
             transform.gameObject.layer = LayerMask.NameToLayer("Default");
         }
-        
+
         odleglosc = player.position - transform.position;
 
-        if(rozwiazano)
+
+        if (Input.GetKeyDown(KeyCode.Escape) && pokazLaptoka)
         {
+            player.GetComponent<Player>().enabled = true;
+            cam.GetComponent<SC_HeadBobber>().enabled = true;
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+
+        }
+        if (text1.text == "1945" && Input.GetKeyDown(KeyCode.Return)) rozwiazano = true;
+        //if (text1.text == "pup") rozwiaz = true;
+        if (rozwiazano)
+        {
+            text.SetActive(false);
+            if (pokazLaptoka) komunikat2.SetActive(true);
+            else komunikat2.SetActive(false);
+            pokazLaptoka2 = true;
             salceson.SetActive(false);
         }
     }
 
-    void OnMouseOver()
+    private void LateUpdate()
     {
-        if(odleglosc.magnitude < odlegloscPodnoszenia)
-        {
-            transform.gameObject.layer = LayerMask.NameToLayer("Outline");
-        }
+        if (!pokazLaptoka) schowajFunkcja();
+        else if (pokazLaptoka) pokazFunkcja();
 
-        mouseOver = true;
-
-        if(odleglosc.magnitude < odlegloscPodnoszenia && Input.GetKeyDown(KeyCode.E) && !rozwiazano)
-        {
-            Uj.SetActive(true);
-            komunikat2.SetActive(false);
-        }
+        if (pokazLaptoka) player.transform.localRotation = Quaternion.Euler(0, 0, 0);
     }
 
-    void OnMouseExit()
+    void pokazFunkcja()
     {
-        mouseOver = false;
+        Uj.SetActive(true);
+        text.SetActive(true);
+        if (pokazLaptoka2){
+            komunikat1.SetActive(false);
+
+        }
     }
+    void schowajFunkcja()
+    {
+        Uj.SetActive(false);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // void Update()
+    // {
+    //     if(odleglosc.magnitude > odlegloscPodnoszenia || !mouseOver)
+    //     {
+    //         transform.gameObject.layer = LayerMask.NameToLayer("Default");
+    //     }
+        
+    //     odleglosc = player.position - transform.position;
+
+    //     if(rozwiazano)
+    //     {
+    //         salceson.SetActive(false);
+    //     }
+    // }
+
+    // void OnMouseOver()
+    // {
+    //     if(odleglosc.magnitude < odlegloscPodnoszenia)
+    //     {
+    //         transform.gameObject.layer = LayerMask.NameToLayer("Outline");
+    //     }
+
+    //     mouseOver = true;
+
+    //     if(odleglosc.magnitude <= odlegloscPodnoszenia && Input.GetKeyDown(KeyCode.E) && !rozwiazano)
+    //     {
+    //         Uj.SetActive(true);
+    //         komunikat2.SetActive(false);
+    //     }
+    // }
+
+    // void OnMouseExit()
+    // {
+    //     mouseOver = false;
+    // }
 }
